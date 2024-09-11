69bac21363ec9dd84fcffcc4c6759025407ffaf1
Flávio Batista dos Santos Menezes (Holding DTI) <DTI.FlavioB@aguiabranca.com.br>
2024-09-11 09:17:59 -0300

    v1

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+appointments = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', appointments=appointments)
+
+@app.route('/schedule', methods=['GET', 'POST'])
+def schedule():
+    if request.method == 'POST':
+        name = request.form['name']
+        service = request.form['service']
+        date = request.form['date']
+        appointments.append({'name': name, 'service': service, 'date': date})
+        return redirect(url_for('index'))
+    return render_template('schedule.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', appointments=appointments)
+
+if __name__ == '__main__':
+    app.run(debug=True)
